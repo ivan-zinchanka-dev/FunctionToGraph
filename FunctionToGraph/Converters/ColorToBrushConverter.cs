6ac2440ad5cfc462f82018834fdbe709823e8d56
JanZinch <jan.zinchanka@mail.ru>
2023-10-08 00:22:01 +0300

    push

---
+++ FunctionToGraph/Converters/ColorToBrushConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Globalization;
+
+namespace FunctionToGraph.Converters;
+
+public class ColorToBrushConverter : IValueConverter {
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+
+        if (value is Color color)
+        {
+            return new SolidColorBrush(color);
+        }
+        else
+        {
+            throw new InvalidOperationException("Value must be a Color");
+        }
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+        
+        throw new NotImplementedException();
+    }
+
+}
--- FunctionToGraph/Views/GraphColorWindow.xaml.cs
+++ FunctionToGraph/Views/GraphColorWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows;
+using System.Windows.Media;
+using ColorPicker.Models;
 
 namespace FunctionToGraph.Views;
 
@@ -7,5 +9,13 @@ public partial class GraphColorWindow : Window
     public GraphColorWindow()
     {
         InitializeComponent();
+    }
+
+    private void OnColorChanged(object sender, RoutedEventArgs e)
+    {
+        Color selectedColor = _colorPicker.SelectedColor;
+        
+        App app = (App)Application.Current;
+        app.LogicalResources["GraphColor"] = selectedColor;
     }
 }
