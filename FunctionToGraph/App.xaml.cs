a2acc5b557db777afdf023276066f7b3d993fc3c
Ivan Zinchanka <75093640+ivan-zinchanka-dev@users.noreply.github.com>
2024-12-02 21:46:13 +0300

    clean: app

--- FunctionToGraph/App.xaml.cs
+++ FunctionToGraph/App.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Threading;
 using System.Windows;
-using System.Windows.Media;
 using FunctionToGraph.Services;
 using FunctionToGraph.Views;
 
@@ -9,42 +8,14 @@ namespace FunctionToGraph
 {
     public partial class App : Application
     {
-        public static App Instance => Current as App;
-        
-        //public AppResourceModel ResourceModel { get; private set; }
-
         private StorageService _storageService;
         private MainWindow _mainWindow;
-        
-        public class AppResourceModel
-        {
-            private readonly ResourceDictionary _resourceDictionary;
-            private const string GraphColorKey = "GraphColor";
-            
-            public AppResourceModel(ResourceDictionary resourceDictionary)
-            {
-                _resourceDictionary = resourceDictionary;
-            }
-
-            public Color GraphColor
-            {
-                get => (Color)_resourceDictionary[GraphColorKey];
-                set
-                {
-                    _resourceDictionary[GraphColorKey] = value; 
-                    OnGraphColorChanged?.Invoke(value);
-                }
-            }
-            
-            public event Action<Color> OnGraphColorChanged; 
-        }
         
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
             PreventMultipleStartup();
             
-            //ResourceModel = new AppResourceModel(Resources.MergedDictionaries[0]);
             _storageService = new StorageService();
             
             _mainWindow = new MainWindow(_storageService);
