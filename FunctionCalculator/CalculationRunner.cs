a9bdd5e9d70cc165c91a7eff32297455e66e82bf
Ivan Zinchanka <75093640+ivan-zinchanka-dev@users.noreply.github.com>
2025-01-18 17:51:50 +0300

    feat: console calculator, p,1

---
+++ FunctionCalculator/CalculationRunner.cs
@@ -0,0 +1,16 @@
+﻿using Common.Models;
+
+namespace FunctionCalculator;
+
+public class CalculationRunner
+{
+    public void Run(string expression, string outputDirectory)
+    {
+        ExpressionModel expressionModel = new ExpressionModel("x*(x-2)");
+        
+        bool result = expressionModel.Validate(out string errorMessage);
+
+        Console.WriteLine("Result: " + result);
+        
+    }
+}
---
+++ FunctionCalculator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.CommandLine;
+
+namespace FunctionCalculator;
+
+public static class Program
+{
+    public static async Task<int> Main(string[] args)
+    {
+        RootCommand rootCommand = new RootCommand("This calculator calculates function values");
+
+        Option<string> expressionOption =
+            new Option<string>("--exp", "Specifies the expression, that represents function");
+        rootCommand.AddOption(expressionOption);
+        
+        Option<string> outputDirectoryOption =
+            new Option<string>("--out", "Specifies the output directory");
+        rootCommand.AddOption(outputDirectoryOption);
+
+
+        CalculationRunner calculationRunner = new CalculationRunner();
+        rootCommand.SetHandler(calculationRunner.Run, expressionOption, outputDirectoryOption);
+        
+        return await rootCommand.InvokeAsync(args);
+    }
+
+}
