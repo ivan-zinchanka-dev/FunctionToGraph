6a6e3c27fbdb87076489d7e68373bd77bf16a3f8
Ivan Zinchanka <75093640+ivan-zinchanka-dev@users.noreply.github.com>
2025-01-20 23:06:07 +0300

    test: reading

--- FunctionCalculator/CalculationRecord.cs
+++ FunctionCalculator/CalculationRecord.cs
@@ -1,18 +1,56 @@
-﻿using CsvHelper.Configuration.Attributes;
+﻿using System.Drawing;
+using System.Globalization;
+using CsvHelper.Configuration.Attributes;
 
 namespace FunctionCalculator;
 
+[CultureInfo("InvariantCulture")]
 public class CalculationRecord
 {
-    [Index(0), Name("X")]
+    [Index(0), Name("Expression"), Optional]
+    public string Expression { get; set; }
+    
+    [Index(1), Name("X")]
     public double XValue { get; set; }
     
-    [Index(1), Name("Y")]
+    [Index(2), Name("Y")]
     public double YValue { get; set; }
+
+    [Ignore]
+    public Color GraphColor { get; set; } = Color.Empty;
+
+    [Index(3), Name("Graph color"), Optional] 
+    public string ConvertedGraphColor => ConvertColor(GraphColor);
+
+    public CalculationRecord()
+    {
+        Expression = string.Empty;
+        XValue = 0;
+        YValue = 0;
+    }
+    
+    public CalculationRecord(string expression, double xValue, double yValue, Color graphColor)
+    {
+        Expression = expression;
+        XValue = xValue;
+        YValue = yValue;
+        GraphColor = graphColor;
+    }
 
     public CalculationRecord(double xValue, double yValue)
     {
+        Expression = string.Empty;
         XValue = xValue;
         YValue = yValue;
+    }
+
+    private static string ConvertColor(Color color)
+    {
+        return color.IsEmpty ? string.Empty : $"{color.R}, {color.G}, {color.B}";
+    }
+
+    public override string ToString()
+    {
+        return $"({Expression}, {XValue.ToString(CultureInfo.InvariantCulture)}, {YValue.ToString(CultureInfo.InvariantCulture)}, {ConvertedGraphColor})";
     }
 }
--- FunctionCalculator/CalculationRunner.cs
+++ FunctionCalculator/CalculationRunner.cs
@@ -1,11 +1,14 @@
-﻿using System.Globalization;
+﻿using System.Drawing;
+using System.Globalization;
 using Common.Models;
 using CsvHelper;
+using CsvHelper.Configuration;
 
 namespace FunctionCalculator;
 
 public class CalculationRunner
 {
+    // TODO use params
     // TODO use async
     
     public void Run(string expression, string outputDirectory)
@@ -16,26 +19,58 @@ public class CalculationRunner
 
         Console.WriteLine("Result: " + result);
 
-        using (StreamWriter streamWriter = new StreamWriter("table.csv"))
+        GraphModel graphModel = new GraphModel(
+            expressionModel.FullExpressionString,
+            expressionModel.XValues,
+            expressionModel.YValues,
+            Color.Red);
+
+        using (StreamWriter streamWriter = new StreamWriter("table.csv", false))
         {
             using (CsvWriter csvWriter = new CsvWriter(streamWriter, CultureInfo.InvariantCulture))
             {
-                csvWriter.WriteRecords(
-                    ToCalculationRecords(expressionModel.XValues, expressionModel.YValues));
+                //csvWriter.WriteComment("This is a metadata ta ta dada tata\n");
+                csvWriter.WriteRecords(ToCalculationRecords(graphModel));
                 csvWriter.Flush();
             }
         }
 
+
+        /*CsvConfiguration config = CsvConfiguration.FromAttributes<CalculationRecord>();
+        
+        using (StreamReader streamReader = new StreamReader("in.csv"))
+        {
+            using (CsvReader csvReader = new CsvReader(streamReader, config))
+            {
+                IEnumerable<CalculationRecord> records = csvReader.GetRecords<CalculationRecord>();
+
+                foreach (var record in records)
+                {
+                    Console.WriteLine(record);
+                }
+            }
+        }*/
+
         Console.WriteLine("CSV ready");
     }
 
-    private IEnumerable<CalculationRecord> ToCalculationRecords(double[] xValues, double[] yValues)
+    private IEnumerable<CalculationRecord> ToCalculationRecords(GraphModel graphModel)
     {
-        CalculationRecord[] records = new CalculationRecord[Math.Min(xValues.Length, yValues.Length)];
-
-        for (int i = 0; i < records.Length; i++)
+        CalculationRecord[] records = new CalculationRecord[
+            Math.Min(graphModel.XValues.Length, graphModel.YValues.Length)];
+        
+        if (records.Length > 0)
         {
-            records[i] = new CalculationRecord(xValues[i], yValues[i]);
+            records[0] = new CalculationRecord(
+                graphModel.Expression, 
+                graphModel.XValues[0], 
+                graphModel.YValues[0],
+                graphModel.Color);
+        }
+        
+        for (int i = 1; i < records.Length; i++)
+        {
+            records[i] = new CalculationRecord(graphModel.XValues[i],  graphModel.YValues[i]);
         }
 
         return records;
