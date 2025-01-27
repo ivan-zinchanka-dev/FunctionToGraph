ae1d11324ed26a16a99cb342820268680121b4d1
Ivan Zinchanka <75093640+ivan-zinchanka-dev@users.noreply.github.com>
2025-01-27 16:19:18 +0300

    feat: async reading

--- Domain/Csv/CsvService.cs
+++ Domain/Csv/CsvService.cs
@@ -5,7 +5,7 @@ namespace Domain.Csv;
 public class CsvService
 {
     private const char Separator = ',';
-     
+    
     public DataTable ReadData(string filePath)
     {
         DataTable data = new DataTable();
@@ -16,22 +16,54 @@ public class CsvService
 
             if (line != null)
             {
-                string[] headers = line.Split(Separator);
-
-                foreach (string header in headers)
-                {
-                    data.Columns.Add(header);
-                }
+                ReadHeaders(line, data.Columns);
             }
 
             while ((line = reader.ReadLine()) != null)
             {
-                string[] fields = line.Split(Separator);
-                data.Rows.Add(fields);
+                ReadFields(line, data.Rows);
             }
         }
 
         return data;
+    }
+    
+    public async Task<DataTable> ReadDataAsync(string filePath)
+    {
+        DataTable data = new DataTable();
+        
+        using (StreamReader reader = new StreamReader(filePath))
+        {
+            string line = await reader.ReadLineAsync();
+
+            if (line != null)
+            {
+                ReadHeaders(line, data.Columns);
+            }
+
+            while ((line = await reader.ReadLineAsync()) != null)
+            {
+                ReadFields(line, data.Rows);
+            }
+        }
+
+        return data;
+    }
+    
+    private void ReadHeaders(string line, DataColumnCollection dataColumns)
+    {
+        string[] headers = line.Split(Separator);
+
+        foreach (string header in headers)
+        {
+            dataColumns.Add(header);
+        }
+    }
+
+    private void ReadFields(string line, DataRowCollection dataRows)
+    {
+        string[] fields = line.Split(Separator);
+        dataRows.Add(fields);
     }
     
     public void WriteData(string filePath, DataTable data, bool append = false)
